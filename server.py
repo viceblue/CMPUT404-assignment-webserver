cf42278bef08cf2861d95f60f08483b1f417b708
viceblue <zhuhang0724@gmail.com>
2017-01-21 20:46:28 -0700

    test

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
     SocketServer.TCPServer.allow_reuse_address = True
+    
     # Create the server, binding to localhost on port 8080
     server = SocketServer.TCPServer((HOST, PORT), MyWebServer)
 
